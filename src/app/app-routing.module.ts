c739b7d71a2581c7009273397eb6f9f20f4c26b4
Marcela vieira Freire <marcelavie@MacBook-Pro-de-Marcela.local>
2022-05-09 19:43:07 -0300

    estrutura do projeto em andamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { GithubSearchComponent } from "./pages/github-user/github-search/github-search.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: GithubSearchComponent },
+  { path: '',   redirectTo: '/search', pathMatch: 'full' },
+  // {
+  //   path: "search",
+  //   loadChildren: () =>
+  //     import("./pages/github-user/github-user.module").then(
+  //       (m) => m.GithubUserModule
+  //     ),
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/github-user/github-user.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GithubSearchComponent } from './github-search/github-search.component';
+
+
+
+@NgModule({
+  declarations: [
+    GithubSearchComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GithubUserModule { }
