7054435d02dd97ec1e27672fef1d89dde32db70d
Marcela vieira Freire <marcelavie@MacBook-Pro-de-Marcela.local>
2022-05-14 15:21:17 -0300

    filtros finalizados

--- src/app/pages/github-user/search/search.component.ts
+++ src/app/pages/github-user/search/search.component.ts
@@ -10,48 +10,61 @@ import { GithubSearchService } from "src/app/shared/services/github/github-searc
 })
 export class SearchComponent implements OnInit {
   username: string;
-  githubRepos: GithubRepos[] = []
-  githubReposFilter: GithubRepos[] = []
+  githubRepos: GithubRepos[] = [];
+  githubReposFilter: GithubRepos[] = [];
   repoName: string;
+  repStars: number;
   loading: boolean;
 
   constructor(private githubService: GithubSearchService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   getUser() {
     this.loading = true;
 
     // setTimeout(() => {
-      this.githubService.getUser(this.username).subscribe({
-        next: (user: GithubUser) => {
-          this.githubService.getRepositories(user.repos_url).subscribe({
-            next: (repos) => {
-              this.githubRepos = repos
-              this.githubReposFilter = repos
-            },
-            error: () => {},
-            complete: () =>  this.loading = false,
-          })
-        },
-        error: () => {},
-        complete: () => {}
-      }) 
+    this.githubService.getUser(this.username).subscribe({
+      next: (user: GithubUser) => {
+        this.githubService.getRepositories(user.repos_url).subscribe({
+          next: (repos) => {
+            this.githubRepos = repos;
+            this.githubReposFilter = repos;
+          },
+          error: () => {},
+          complete: () => (this.loading = false),
+        });
+      },
+      error: () => {},
+      complete: () => {},
+    });
     // }, 100);
   }
 
   orderRepos(sortType: string) {
-    const reps = this.githubReposFilter
-    if(sortType === 'name') {
-      this.githubRepos = [...reps].sort((a, b) => a.name !== b.name ? a.name < b.name ? -1 : 1 : 0)
+    const reps = this.githubReposFilter;
+    if (sortType === "name") {
+      this.githubRepos = [...reps].sort((a, b) =>
+        a.name !== b.name ? (a.name < b.name ? -1 : 1) : 0
+      );
     } else {
-      this.githubRepos = [...reps].sort((a, b) => a.stargazers_count - b.stargazers_count);
+      this.githubRepos = [...reps].sort(
+        (a, b) => a.stargazers_count - b.stargazers_count
+      );
     }
   }
 
-  searchRepoBy() {
-    const reps = this.githubReposFilter
-    this.githubRepos = reps.filter((rep) =>  rep.name.toLowerCase().includes(this.repoName.toLowerCase()))
+  searchRepoByName() {
+    const reps = this.githubReposFilter;
+    this.githubRepos = reps.filter((rep) =>
+      rep.name.toLowerCase().includes(this.repoName.toLowerCase())
+    );
+  }
+
+  searchRepoByStars() {
+    const reps = this.githubReposFilter;
+    this.githubRepos = reps.filter(
+      (rep) => rep.stargazers_count == this.repStars
+    );
   }
 }
